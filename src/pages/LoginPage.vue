35b4fc1e7735ec7a5f1a37059d5f37d325123431
kitoken99 <kitoken99@gmail.com>
2023-11-02 16:07:52 +0900

    login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -6,10 +6,10 @@
     <div class="row">
       <q-card square bordered class="q-pa-lg shadow-1">
         <q-card-section class="column">
-          <LoginForm />
+          <!-- <LoginForm /> -->
         </q-card-section>
         <q-card-section>
-          <SocialLoginBottuns />
+          <!-- <SocialLoginBottuns /> -->
         </q-card-section>
         <q-card-section class="text-center q-pa-none">
           <p class="text-grey-6">
@@ -23,11 +23,11 @@
 </template>
 
 <script>
-import LoginForm from "src/components/auth/LoginForm.vue";
-import SocialLoginBottuns from "src/components/auth/SocialLoginBottuns.vue";
+// import LoginForm from "src/components/auth/LoginForm.vue";
+// import SocialLoginBottuns from "src/components/auth/SocialLoginBottuns.vue";
 export default {
   name: "LoginPage",
-  components: { LoginForm, SocialLoginBottuns },
+  // components: { LoginForm, SocialLoginBottuns },
 };
 </script>
 
