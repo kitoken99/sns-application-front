4a49f801100c1db5e0aee2f3ce4b2324890d1140
kitoken99 <kitoken99@gmail.com>
2023-11-15 15:56:46 +0900

    iconの修正

--- src/components/home/icons/IconBottunWithText.vue
+++ src/components/home/icons/IconBottunWithText.vue
@@ -2,7 +2,11 @@
   <div>
     <q-btn round>
       <div class="inline rounded-borders cursor-pointer icon-circle">
-        <q-avatar color="grey-4" text-color="black" :icon="icon" />
+        <q-avatar color="grey-4" text-color="black" :icon="icon"  v-if="store.getters['state/getIsFetched']"/>
+        <q-skeleton
+          v-if="!store.getters['state/getIsFetched']"
+          type="QAvatar"
+        />
         <q-tooltip> {{ text }} </q-tooltip>
       </div>
     </q-btn>
@@ -11,6 +15,7 @@
 
 <script>
 import { defineComponent } from "vue";
+import { useStore } from "vuex";
 export default defineComponent({
   name: "AvatarBottunWithText",
   props: {
@@ -22,6 +27,12 @@ export default defineComponent({
       type: String,
       required: true,
     },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+    };
   },
 });
 </script>
